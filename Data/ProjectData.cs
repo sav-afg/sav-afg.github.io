08e700f9972af069be579978a7add68b6abea03c
sav-afg <s_hakimzada@hotmail.com>
2026-02-03 18:01:04 +0000

    Minor refinements

--- Data/ProjectData.cs
+++ Data/ProjectData.cs
@@ -11,6 +11,20 @@ public static class ProjectData
         return
         [
             new() {
+                Id = 1,
+                Title = "Service Booking Platform",
+                Description = "A secure dual-token authentication system using JWT access tokens and rotating refresh tokens that allows users to book services.",
+                Technologies = ".NET 10, ASP.NET Core, SQL Server, C#",
+                GitHubUrl = "https://github.com/sav-afg/ServiceBookingPlatform",
+                LiveUrl = "",
+                Feature1 = "Implemented claims-based authorisation to enforce user ownership and role-specific access (Customer, Staff, Admin).",
+                Feature2 = "Developed 80 unit tests covering authentication, booking rules, refresh token rotation, and authorization logic",
+                Feature3 = "Built booking conflict detection logic to prevent overlapping reservations using time-range validation.",
+                Feature4 = "Structured the API using Controller–Service–DTO separation, enforcing clean architecture and testability.",
+                ImagePath1 = "/Images/Projects/Service-Booking-Platform/BookingConflict.png",
+                ImagePath2 = "/Images/Projects/Service-Booking-Platform/LogIn.png"
+            },
+            new() {
                 Id = 2,
                 Title = "Fitness Saviour",
                 Description = "A full-stack fitness and nutrition web application",
@@ -24,20 +38,6 @@ public static class ProjectData
                 ImagePath1 = "/Images/Projects/Fitness-Saviour/WorkoutSplitResults.png",
                 ImagePath2 = "/Images/Projects/Fitness-Saviour/Graph.png",
                 ImagePath3 = "/Images/Projects/Fitness-Saviour/WorkoutSplitQuestionnaire.png"
-            },
-            new() {
-                Id = 1,
-                Title = "Service Booking Platform",
-                Description = "A secure dual-token authentication system using JWT access tokens and rotating refresh tokens that allows users to book services.",
-                Technologies = ".NET 10, ASP.NET Core, SQL Server, C#",
-                GitHubUrl = "https://github.com/sav-afg/ServiceBookingPlatform",
-                LiveUrl = "",
-                Feature1 = "Implemented claims-based authorisation to enforce user ownership and role-specific access (Customer, Staff, Admin).",
-                Feature2 = "Developed 80 unit tests covering authentication, booking rules, refresh token rotation, and authorization logic",
-                Feature3 = "Built booking conflict detection logic to prevent overlapping reservations using time-range validation.",
-                Feature4 = "Structured the API using Controller–Service–DTO separation, enforcing clean architecture and testability.",
-                ImagePath1 = "/Images/Projects/Service-Booking-Platform/BookingConflict.png",
-                ImagePath2 = "/Images/Projects/Service-Booking-Platform/LogIn.png"
             },
             new() {
                 Id = 3,
